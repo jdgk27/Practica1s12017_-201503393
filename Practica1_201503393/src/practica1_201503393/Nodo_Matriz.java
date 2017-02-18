df5055a6749cdb1689d0824ac20631a28bad4547
Daniel De León <Daniel De Len@192.168.56.1>
2017-02-18 00:27:40 -0600

    Creacion de matriz con errores y un 10%

--- Practica1_201503393/src/practica1_201503393/Nodo_Matriz.java
+++ Practica1_201503393/src/practica1_201503393/Nodo_Matriz.java
@@ -13,6 +13,51 @@ public class Nodo_Matriz {
     
     public String letra;
     public int valor;
+    public Nodo_Matriz arriba;
+    public Nodo_Matriz abajo;
+    public Nodo_Matriz izquierda;
+    public Nodo_Matriz derecha;
     
     //public int dimension = MenuPrincipal.dimension;
+    
+    public Nodo_Matriz(int valor) {
+        this.valor = valor;
+        this.arriba = null;
+        this.abajo = null;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+  
+    public String Mostrar() {        
+        
+        String retorno = this.dato+"\n";
+        if (this.adelante != null) {
+            retorno += this.adelante.Mostrar();
+        }
+        return retorno;      
+    }
+
+    public void Insertar(Nodo_Diccio nuevo) {
+        if (this.adelante == null) {
+            this.adelante = nuevo;
+        } else {
+            this.adelante.Insertar(nuevo);
+        }
+    }
+
+    public Object getDato() {
+        return this.dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo_Diccio getAdel() {
+        return adelante;
+    }
+    
+    public void setAdel(Nodo_Diccio adelante){
+        this.adelante=adelante;
+    }
 }
